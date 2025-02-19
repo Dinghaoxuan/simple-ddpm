d5e9b984af2ec5b82b35598d468b08f777108494
dhhx <975494839@qq.com>
2025-02-19 16:44:04 +0800

    update

--- ddpm/diffusion.py
+++ ddpm/diffusion.py
@@ -38,6 +38,57 @@ class GaussianDiffusionTrainer(nn.Module):
         loss = F.mse_loss(self.model(x_t, t), noise, reduction="none")
         return loss
     
+
+class GaussianDiffusionSampler(nn.Module):
+    def __init__(self, model, beta_1, beta_T, T):
+        super().__init__()
+        
+        self.model = model
+        self.T = T
+        
+        self.register_buffer("betas", torch.linspace(beta_1, beta_T, T).double())
+        alphas = 1.0 - self.betas
+        alphas_bar = torch.cumprod(alphas, dim=0)
+        alphas_bar_prev = F.pad(alphas_bar, [1, 0], value=1.0)[:T]
+        
+        self.register_buffer("coeff1", torch.sqrt(1.0 / alphas))
+        self.register_buffer("coeff2", self.coeff1 * (1.0 - alphas) / torch.sqrt(1.0 - alphas_bar))
+        self.register_buffer("posterior_var", self.betas * (1.0 - alphas_bar_prev) / (1.0 - alphas_bar))  #根据beta求的方差
+        
+    def predict_xt_prev_mean_from_eps(self, x_t, t, eps):
+        # x_t -> x_t-1
+        assert x_t.shape == eps.shape
+        return (extract(self.coeff1, t, x_t.shape) * x_t - extract(self.coeff2, t, x_t.shape) * eps)
+    
+    def p_mean_variance(self, x_t, t):
+        var = torch.cat([self.posterior_var[1:2], self.betas[1:]])
+        var = extract(var, t, x_t.shape)
+        
+        eps = self.model(x_t, t) 
+        xt_prev_mean = self.predict_xt_prev_mean_from_eps(x_t, t, eps=eps)
+        
+        return xt_prev_mean, var
+    
+    def forward(self, x_T):
+        x_t = x_T
+        for time_step in reversed(range(self.T)):
+            print(time_step)
+            t = x_t.new_ones([x_T.shape[0], ], dtype=torch.long) * time_step  # [B, ]
+            mean, var = self.p_mean_variance(x_t=x_t, t=t)
+            if time_step > 0:
+                noise = torch.randn_like(x_t)
+            else:
+                noise = 0
+                
+            x_t = mean + torch.sqrt(var) * noise
+            assert torch.isnan(x_t).int().sum() == 0, "nan in tensor."
+        x_0 = x_t
+        return torch.clip(x_0, -1, 1)
+
+    
+    
+    
+        
     
     
     
--- ddpm/train.py
+++ ddpm/train.py
@@ -5,9 +5,13 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 from torchvision import transforms
 
+from torchvision.utils import save_image
+
 from model import UNet
 from scheduler import GradualWarmupScheduler
-from diffusion import GaussianDiffusionTrainer
+from diffusion import GaussianDiffusionTrainer, GaussianDiffusionSampler
+
+import tqdm
 
 
 def train(model_config):
@@ -56,5 +60,52 @@ def train(model_config):
     
     trainer = GaussianDiffusionTrainer(net_model, model_config["beta_1"], model_config["beta_T"], model_config["T"]).to(device)
     
-    
-    
+    for epoch in range(model_config["epoch"]):
+        with tqdm(dataloader, dyanmic_ncols=True) as tqdmDataLoader:
+            for images, labels in tqdmDataLoader:
+                optimizer.zero_grad()
+                x_0 = images.to(device)
+                loss = trainer(x_0).mean()
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(net_model.parameters(), model_config["clip_grad"])
+                optimizer.step()
+                
+                tqdmDataLoader.set_postfix(ordered_dict = {
+                    "epoch": epoch,
+                    "loss": loss.item(),
+                    "image shape": x_0.shape,
+                    "lr": optimizer.state_dict()["param_groups"][0]["lr"],
+                    })
+        warmup_scheduler.step()
+        torch.save(net_model.state_dict(), os.path.join(model_config["save_weight_dir"], f"ckpt_{epoch}.pt"))
+        
+
+def eval(model_config):
+    with torch.no_grad():
+        device = torch.device(model_config["device"])
+        
+        model = UNet(T=model_config["T"], 
+                     dim=model_config["dim"], 
+                     dim_scale=model_config["dim_scale"], 
+                     attn=model_config["attn"],
+                     num_res_blocks=model_config["num_res_blocks"],
+                     dropout=0.).to(device)
+        
+        ckpt = torch.load(os.path.join(model_config["save_weight_dir"], model_config["test_load_weight"]), map_location=device)
+        model.load_state_dict(ckpt)
+        print("model load weight done.")
+        
+        model.eval()
+        
+        sampler = GaussianDiffusionSampler(model, model_config["beta_1"], model_config["beta_T"], model_config["T"]).to(device)
+        
+        noise_image = torch.randn(size=[model_config["batch_size"], 3, model_config["img_size"], model_config["img_size"]], device=device)
+        save_noise = torch.clamp(noise_image * 0.5 + 0.5, 0, 1)
+        
+        save_image(save_noise, os.path.join(model_config["sampled_dir"], model_config["sampled_noise_name"], nrow=model_config["nrow"]))
+        sampled_image = sampler(noise_image)
+        sampled_image = sampled_image * 0.5 + 0.5
+        
+        save_image(sampled_image, os.path.join(model_config["sampled_dir"], model_config["sampled_image_name"], nrow=model_config["nrow"]))
+        
+        
